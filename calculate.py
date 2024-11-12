b77b80561221b5aaa65a7922ae643feeb1e3a27e
Whoamimmm <muhsabirov@ya.ru>
2024-11-12 23:54:25 +0300

    .

--- calculate.py
+++ calculate.py
@@ -1,38 +1,41 @@
+import circle
+import square
+
 figs = ['circle', 'square']
 funcs = ['perimeter', 'area']
-sizes = {
-    'perimeter-circle': 1,
-    'area-circle': 1,
-    'perimeter-square': 1,
-    'area-square': 1,
-}
 
 def calc(fig, func, size):
     if fig not in figs:
-        raise ValueError(f"Invalid figure:
-                         {fig}. Available figures are: {figs}")
+        raise ValueError(f"Invalid figure: {fig}. Available figures are: {figs}")
     if func not in funcs:
-        raise ValueError(f"Invalid function: 
-                         {func}. Available functions are: {funcs}")
+        raise ValueError(f"Invalid function: {func}. Available functions are: {funcs}")
+
     if any(s < 0 for s in size):
-        raise ValueError("Can't use negative values for figure dimensions.")
+        raise ValueError("Negative values are not allowed for figure dimensions.")
 
-    result = eval(f'{fig}.{func}(*{size})')
-    return result
+    if fig == 'circle':
+        if func == 'perimeter':
+            return circle.perimeter(size[0])
+        elif func == 'area':
+            return circle.area(size[0])
+    elif fig == 'square':
+        if func == 'perimeter':
+            return square.perimeter(size[0])
+        elif func == 'area':
+            return square.area(size[0])
 
-if __name__ == "__main__":
-    func = ''
-    fig = ''
-    size = list()
-    while fig not in figs:
-        fig = input(f"Enter figure name, available are {figs}:n")
-    while func not in funcs:
-        func = input(f"Enter function name, available are {funcs}:n")
-    while len(size) != sizes.get(f"{func}-{fig}", 1):
-        size = list(map(int, 
-                        input("Input figure sizes separated by space:n").split(' ')))
-    result = calc(fig, func, size)
-    print(f'{func} of {fig} is {result}')
+try:
+    figure = 'circle'
+    function = 'area'
+    dimensions = [5]
+    result = calc(figure, function, dimensions)
+    print(f"The {function} of the {figure} with dimensions {dimensions} is: {result}")
 
+    figure = 'square'
+    function = 'perimeter'
+    dimensions = [4]
+    result = calc(figure, function, dimensions)
+    print(f"The {function} of the {figure} with dimensions {dimensions} is: {result}")
 
-
+except ValueError as e:
+    print(e)
